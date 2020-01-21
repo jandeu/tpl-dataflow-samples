ccb5126bed1a24d9615fed8c1678b1d0da202924
jandeu <jan.deutschl@outlook.cz>
2020-01-21 21:39:20 +0100

    Initial samples commit

---
+++ Sample1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var actionBlock = new ActionBlock<string>(input =>
+            {
+                Thread.Sleep(r.Next(100, 2000));
+                Console.WriteLine($"Done: {input}, thread: {Thread.CurrentThread.ManagedThreadId}");
+            }, new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = 1, EnsureOrdered = true });
+
+            for (int i = 0; i < 1000; i++)
+            {
+                // Thread.Sleep(r.Next(100, 200));
+                var msg = $"msg_{i}";
+
+                Console.WriteLine($"Adding {i}");
+                actionBlock.Post(msg);
+            }
+
+            Console.WriteLine("action block input count: " + actionBlock.InputCount);
+
+            //while (true)
+            //{
+            //    Thread.Sleep(1000);
+            //}            
+        }
+    }
+}
---
+++ Sample4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Sample4
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            int fn(int input) { Console.WriteLine(input); return input + 1; }
+
+            var step1 = new TransformBlock<int, int>(fn);
+            var step2 = new TransformBlock<int, int>(fn);
+            var step3 = new ActionBlock<int>(i=> { });
+
+            step1.LinkTo(step2);
+            step2.LinkTo(step3);
+
+            _ = step1.Completion
+              .ContinueWith(_ =>
+              {
+                  Console.WriteLine("step 1 complete");
+                  step2.Complete();
+              });
+
+            _ = step2.Completion.ContinueWith(_ =>
+            {
+                Console.WriteLine("step 2 complete");
+                step3.Complete();
+            });
+            step1.Post(1);
+            step1.Complete();
+            
+            await step3.Completion;
+            Console.WriteLine("done");
+        }
+    }
+}
